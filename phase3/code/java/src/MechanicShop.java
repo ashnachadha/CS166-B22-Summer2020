ab35ac0a0401050b430621867eb8343591272387
Ashna Chadha <achad006@wch132-25.cs.ucr.edu>
2020-08-20 12:58:09 -0700

    Add modified MechanicShop

--- phase3/code/java/src/MechanicShop.java
+++ phase3/code/java/src/MechanicShop.java
@@ -305,15 +305,240 @@ public class MechanicShop{
 	}//end readChoice
 	
 	public static void AddCustomer(MechanicShop esql){//1
-		
+		String first_name;
+
+		do {    
+        		System.out.print("Enter First name: ");
+       		 	try {   
+                		first_name = in.readLine(); 
+                		if(first_name.length() <= 0 || first_name.length() > 32) {
+                			throw new RuntimeException("First name cannot be null or exceed 32 characters");
+        			}break;
+        
+        		}catch (Exception e) {
+        			System.out.println (e);
+        			continue;
+        		}
+		}while (true);
+
+		String last_name;
+
+		do {    
+        		System.out.print("Enter Last name: ");
+        		try {   
+                		last_name = in.readLine(); 
+                		if(last_name.length() <= 0 || last_name.length() > 32) {
+                			throw new RuntimeException("Last name cannot be null or exceed 32 characters");
+        			}break;
+        
+        		}catch (Exception e) {
+        			System.out.println(e);
+        			continue;
+        		}
+		}while (true);
+
+		int ID;
+
+        	do {
+                	System.out.print("Enter Customer ID: ");
+        		try {
+                		ID = Integer.parseInt(in.readLine());
+                		if(ID <= 0) throw new RuntimeException("Customer ID cannot be null");
+        			break;
+        		}catch (Exception e) {
+                		System.out.println(e);
+                		continue;
+        		}
+		}while (true);
+
+		String phone_num;
+
+		do {    
+        		System.out.print("Enter Phone number: ");
+        		try {   
+                		phone_num = in.readLine(); 
+                		if(phone_num.length() <= 0 || phone_num.length() > 13) {
+                			throw new RuntimeException("Phone number cannot be null or exceed 13 characters");
+        			}break;
+        
+        		}catch (Exception e) {
+        			System.out.println(e);
+        			continue;
+        		}
+		}while (true);
+
+		String address;
+
+		do {
+        		System.out.print("Enter Address: ");
+        		try {
+                		address = in.readLine();
+                		if(address.length() <= 0 || address.length() > 256) {
+                			throw new RuntimeException("Last name cannot be null or exceed 256 characters");
+
+        			}
+				break;
+        		}catch (Exception e) {
+        			System.out.println(e);
+        			continue;
+        		}
+		}while (true);
+
+		try {
+      			String query = "INSERT INTO Customer (id, fname, lname, phone, address) VALUES (" + ID + ", \'" + first_name + "\', \'" + last_name + "\',  \'" + phone_num  + "\',  \'" + address + "\' );";
+
+                        esql.executeUpdate(query);
+                }catch (Exception e) {
+                        System.err.println (e.getMessage());
+                }
 	}
 	
 	public static void AddMechanic(MechanicShop esql){//2
-		
+		int ID;
+
+	        do {
+                	System.out.print("Enter Employee ID: ");
+                	try {
+                        	ID = Integer.parseInt(in.readLine());
+                        	if(ID <= 0) throw new RuntimeException("Employee ID cannot be null");
+                        	break;
+                	}catch (Exception e) {
+                        	System.out.println(e);
+                        	continue;
+                	}
+        	}while (true);
+
+
+        	String first_name;
+
+        	do {
+                	System.out.print("Enter First name: ");
+                	try {
+                        	first_name = in.readLine();
+                        	if(first_name.length() <= 0 || first_name.length() > 32) {
+                                	throw new RuntimeException("First name cannot be null or exceed 32 characters");
+                        	}break;
+
+                	}catch (Exception e) {
+                        	System.out.println (e);
+                        	continue;
+                	}
+        	}while (true);
+
+        	String last_name;
+
+        	do {
+                	System.out.print("Enter Last name: ");
+                	try {
+                        	last_name = in.readLine();
+                        	if(last_name.length() <= 0 || last_name.length() > 32) {
+                        		throw new RuntimeException("Last name cannot be null or exceed 32 characters");
+                		}break;
+
+                	}catch (Exception e) {
+                        	System.out.println(e);
+                        	continue;
+                	}
+        	}while (true);
+
+        	int experience;
+        
+        	do{
+                	System.out.print("Enter employee's experience(number of years): "
+                	try{
+            
+                        	experience = Integer.parseInt(in.readLine());
+                        	if(experience < 0 || experience >= 100) throw new RuntimeException("Employee's experience cannot be null or exceed 100 years");
+                        	break;
+                	}catch (Exception e) {
+                        	System.out.println(e);
+                        	continue;       
+                	}               
+        	}while (true);          
+                        
+        	try {                   
+                	String query = "INSERT INTO Mechanic (id, fname, lname, experience) VALUES (" + ID + ", \'" + first_name + "\', \'" + last_name + "\',  " + experience  + " );";
+                        
+                        esql.executeUpdate(query);
+        	}catch (Exception e) {
+                	System.err.println (e.getMessage());
+        	}
 	}
 	
 	public static void AddCar(MechanicShop esql){//3
-		
+		String in1;
+                do{
+                        System.out.print("\tEnter VIN: ");
+                        try{
+                                in1 = in.readLine();
+                                if(in1.length() <= 0 || in1.length() > 16) {
+                                        throw new RuntimeException("VIN cannot be null or exceed 16 characters");
+                                }
+                                break;
+                        } catch(Exception e) {
+                                System.out.println("Your input is invalid!");
+                                continue;
+                        }
+                } while(true);
+
+                String in2;
+                do{
+                        System.out.print("\tEnter make: ");
+                        try{
+                                in2 = in.readLine();
+                                if(in2.length() <=0 || in2.length() > 32) {
+                                        throw new RuntimeException("Make cannot be null or exceed 32 characters");
+                                }
+                                break;
+                        } catch(Exception e) {
+                                System.out.println("Your input is invalid!");
+                                continue;
+                        }
+                } while(true);
+
+                String in3;
+                do{
+                        System.out.print("\tEnter model: ");
+                        try{
+                                in3 = in.readLine();
+                                if(in3.length() <= 0 || in3.length() > 32) {
+                                        throw new RuntimeException("Model cannot be null or exceed 32 characters");
+                                }
+                                break;
+                        } catch(Exception e) {
+                                System.out.println("Your input is invalid!");
+                                continue;
+                        }
+                } while(true);
+
+ 		int in4;
+                do{
+                        System.out.print("\tEnter year: ");
+                        try{
+                                in4 = Integer.parseInt(in.readLine());
+                                if(in4 < 1970) {
+                                        throw new RuntimeException("Year cannot be less than 1970");
+                                }
+                                break;
+                        } catch(Exception e) {
+                                System.out.println("Your input is invalid!");
+                                continue;
+                        }
+                } while(true);
+
+                try{
+                        String query = "INSERT INTO Car(vin, make, model, year) VALUES(\'" + in1 + "\',\'" + in2 + "\',\'" + in3 + "\'," + in4 +")";
+                        esql.executeUpdate(query);
+                } catch(Exception e) {
+                        System.err.println(e.getMessage());
+                }
+
+		try{
+			String query2 = "SELECT * FROM Car WHERE vin = ";
+			query2 += in1;
+
+			esql.executeQuery(query2);
+
 	}
 	
 	public static void InsertServiceRequest(MechanicShop esql){//4
@@ -325,25 +550,66 @@ public class MechanicShop{
 	}
 	
 	public static void ListCustomersWithBillLessThan100(MechanicShop esql){//6
-		
+		try{
+                        String query = "SELECT c.fname AS FirstName, c.lname AS LastName, b.bill FROM Customer c, Service_Request a, Closed_Request b WHERE c.id = a.customer_id AND a.rid = b.rid AND b.bill < 100;";
+                        int rowCount = esql.executeQueryAndPrintResult(query);
+                        System.out.println("total row(s): " + rowCount);
+                }
+                catch(Exception e){
+                        System.err.println(e.getMessage());
+                }
+
 	}
 	
 	public static void ListCustomersWithMoreThan20Cars(MechanicShop esql){//7
-		
+		try{
+			String query = "SELECT fname, lname FROM Customer WHERE id IN (SELECT customer_id FROM Owns GROUP BY customer_id HAVING COUNT(customer_id) > 20)";
+			
+			int rowCount = esql.executeQuery(query);
+			System.out.println("total row(s): " + rowCount);
+
+			esql.executeQueryAndPrintResult(query);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 	
 	public static void ListCarsBefore1995With50000Milles(MechanicShop esql){//8
-		
+		try{
+                        String query = "SELECT DISTINCT make, model, year FROM Car AS C, Service_Request AS S WHERE year < 1995 and S.car_vin = C.vin and S.odometer < 50000;";
+                        int rowCount = esql.executeQueryAndPrintResult(query);
+                        System.out.println("total row(s): " + rowCount);
+                }
+                catch(Exception e){
+                        System.err.println(e.getMessage());
+                }
 	}
 	
 	public static void ListKCarsWithTheMostServices(MechanicShop esql){//9
-		//
-		
+		 try{
+                        String query = "SELECT make, model, a.num_requests FROM Car c, (SELECT car_vin, COUNT(rid) AS num_requests FROM Service_Request GROUP BY car_vin ) AS a WHERE a.car_vin = c.vin ORDER BY a.num_requests DESC LIMIT "    ;
+                        System.out.println("Enter the number of cars you want to view: ");
+                        String num = in.readLine();
+                        query += num + ";";
+
+                        int rowCount = esql.executeQueryAndPrintResult(query);
+                        System.out.println("total row(s): " + rowCount);
+                }
+                catch(Exception e){
+                        System.err.println(e.getMessage());
+                }
+
 	}
 	
 	public static void ListCustomersInDescendingOrderOfTheirTotalBill(MechanicShop esql){//9
-		//
-		
+		try{
+                        String query = "SELECT C.fname, C.lname, total FROM Customer AS C,(SELECT SR.customer_id, SUM(CR.bill) AS total FROM Closed_Request AS CR, Service_Request AS SR WHERE CR.rid = SR.rid GROUP BY SR.customer_id) AS B WHERE C.id=B.customer_id ORDER BY B.total DESC;";
+                        int rowCount = esql.executeQueryAndPrintResult(query);
+                        System.out.println("total row(s): " + rowCount);
+                }
+                catch(Exception e){
+                        System.err.println(e.getMessage());
+                }	
 	}
 	
 }
